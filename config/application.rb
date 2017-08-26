5ff727bc1df858ac5eee9eb6f622d672ebecec3b
yudaionodera <yuudai.2451@gmail.com>
2017-08-27 08:57:29 +0900

    edit-application.rb
    WHAT
    application.rbの編集
    WHY
    rails gにより生まれる無駄なファイルを作成しないため。

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+  	#...
+  	config.generators do |g|
+  		g.assets false
+  		g.helper false
+  		g.javascripts false
+  		g.test_framework false
+  	end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
