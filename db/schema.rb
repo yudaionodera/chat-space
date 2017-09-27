b090f5c78983359f1ff543feaf76a0da55f3f4bf
yudaionodera <onoderayuudai@onoderayudaiMacBook-Pro.local>
2017-09-27 18:36:17 +0900

    Devise-サインアップ機能の追加
    WHAT
    Deviseの追加カラムを追加できるようにした。
    WHY
    devise実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927090048) do
+ActiveRecord::Schema.define(version: 20170927093402) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                   default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170927090048) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "password_confirmation"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
