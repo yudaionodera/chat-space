707daf6a7ebed03300bce469c472bd09573442ac
yudaionodera <onoderayuudai@onoderayudaiMacBook-Pro.local>
2017-10-04 21:07:18 +0900

    フラッシュメッセージの日本語化
    WHAT
    フラッシュメッセージを日本語化するために、i18nを入れた
    WHY
    ユーザー機能実装のため。

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 
 module ChatSpace
   class Application < Rails::Application
@@ -14,6 +14,7 @@ module ChatSpace
   		g.helper false
   		g.javascripts false
   		g.test_framework false
+      config.i18n.default_locale = :ja
   	end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -22,6 +23,7 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_frameworks false
+      config.i18n.default_locale = :ja
     end
   end
 end
