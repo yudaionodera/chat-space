ff2df300722b83cdf095111abc60ac11a1491816
yudaionodera <onoderayuudai@onoderayudaiMacBook-Pro.local>
2017-10-10 20:10:11 +0900

    deviseの日本語化
    WHAT
    deviseの日本語化
    WHY
    devise実装

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,22 @@ module ChatSpace
   		g.test_framework false
       config.i18n.default_locale = :ja
   	end
+
+    # config.before_configuration do
+    #   I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    #   I18n.locale = :ja
+    #   I18n.default_locale = :ja
+    #   config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    #   config.i18n.locale = :ja
+    #   # bypasses rails bug with i18n in production
+    #   I18n.reload!
+    #   config.i18n.reload!
+    # end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
+    config.i18n.locale = :ja
     config.time_zone = 'Tokyo'
     config.generators do |g|
       g.javascripts false
