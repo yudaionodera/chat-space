317f7e5cbeddbd225dbf32d03d17a206dbbcb211
yudaionodera <onoderayuudai@onoderayudaiMacBook-Pro.local>
2017-10-25 19:18:00 +0900

    コントローラのアクション設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,16 @@ class MessagesController < ApplicationController
 	def index
 			@group = current_user.groups
 	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
 end
