602d371f906a21953db3e055a21f3baf37cc16da
yudaionodera <onoderayuudai@onoderayudaiMacBook-Pro.local>
2017-11-04 16:01:36 +0900

    CarrierWave設定
    WAHT
    CarrierWaveを使えるようにした
    WHY
    画像投稿を実装したかったため。

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,6 @@ class Message < ApplicationRecord
 
   belongs_to :group
   belongs_to :user
+
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025101545) do
+ActiveRecord::Schema.define(version: 20171104053639) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 20171025101545) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_models_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.text     "image",      limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
