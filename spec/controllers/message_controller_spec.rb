866a944461c11ae76f16675e4dcfb3ff46540aac
yudaionodera <yuudai.2451@gmail.com>
2017-11-26 20:29:43 +0900

    テストコード修正

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -1,71 +1,91 @@
 require 'rails_helper'
+require 'devise'
 
-describe MessageController, type: :controller do
+describe MessagesController, type: :controller do
   let(:user){create(:user)}
+  let(:group){create(:group)}
 
   describe 'GET #index' do
-    # context 'ログインしている際' do
-      before  do
-        login_user user
-        # controller_macros.rb内のlogin_userメソッドを呼び出し
-      end
+     context 'with user login' do
+        before  do
+          login_user user # controller_macros.rb内のlogin_userメソッドを呼び出し
+        end
+        it "assigns the requested message to @groups" do
+          groups = create_list(:group, 3)
+          get :index
+          expect(assigns(:groups)).to match(groups)
+        end
 
-      it "assigns the requested message to @groups" do
-        groups = create_list(:grouo, 3)
-        get :index
-        expect(assigns(:groups)).to match(groups)
-      end
+        it "assigns the requested message to @group" do
+          #group = FactoryGirl.create(:group)
+          get :index id: #group#id: group.uesr_id
+          expect(assaigns(:group)).to eq group
+        end
 
-      it "assigns the requested message to @group" do
-        group = FactoryGirl.create(:grouo)
-        get :index id: grouo
-        expect(assaigns(:group)).to eq group
-      end
+        it "assigns the requested message to @messages" do
+          messages = create_list(:message, 3)
+          get :index
+          expect(assigns(:messages)).to match(messages)
+        end
 
-      it "assigns the requested message to @messages" do
-        messages = create_list(:message, 3)
-        get :index
-        expect(assigns(:messages)).to match(messages)
-      end
+        it "assigns the requested @message" do
+          message = FactoryGirl.create(:message)
+          get :index #id: message_id
+          expect(assaigns(:message)).to be_a_new(Message)
+        end
 
-      it "assigns the requested @message" do
-      end
-
-      it "it renders the :index template" do
-        get :index
-        expect(response).to render_template :index
-      end
-    context 'ログインしていない場合' do
-      it "意図したビューに(devise/session/new)リダイレクトされるか" do
+        it "it renders the :index template" do
+          get :index
+          expect(response).to render_template :index
+        end
+      context 'by no login user' do
+        before do
+            patch :create, permalink: user.permalink, user: attributes_for(:user, name: '  ')
+          end
+        it "it renders the :devise/session/new template" do
+          expect{
+            patch :create, permalink: 'hogehoge' , user: attributes_for(:user)
+          }.to raise_exception(ActiveRecord::RecordNotFound) #ここどうかけばいいんだ
+        end
       end
     end
   end
 
   describe 'POST #create'
-    context 'ログインしているかつ、保存に成功した際' do
-      it "メッセージの保存はできているか" do
+    context 'uesr is logind and succsessed save the message' do
+      before do
+        login_user user
+      end
+      it "has saved message in the database" do
         message = FactoryGirl.create(:message)
-        get :index id: message
+        get :index #id: message
         expect(assaigns(:message)).to eq message
       end
-    context  'ログインしているかつ、保存に失敗した際' do
+    context  'uesr is logind and faild save the message' do
       it "renders the :index template" do
-        message = FactoryGirl.create((:message, body: "",image: "" ))
-        get :index id: message
+        message = FactoryGirl.create(:message, body: "",image: "" )
+        get :index #id: message
         #ほげほげ。失敗した時の反応
         # 失敗した時の記入の仕方どうやるんだろう。
       end
 
       it "it renders the root template after message create" do
         message = FactoryGirl.create(:message)
-        get:index, id: message.id
+        get:index  #id: message.id
         expect(response).to render_template :root
       end
     end
-    context 'ログインしていない場合' do
+    context 'by no login user' do
+      before do
+          patch :create, permalink: user.permalink, user: attributes_for(:user, name: '  ')
+        end
       it "it renders the :devise/session/new template" do
-        get :devise/session/new
-        # ほげほげ
+        expect{
+          patch :create, permalink: 'hogehoge' , user: attributes_for(:user)
+        }.to raise_exception(ActiveRecord::RecordNotFound)#ここどうかけばいいんだ
     end
   end
 end
+end
+
+#before do と contextmのp
