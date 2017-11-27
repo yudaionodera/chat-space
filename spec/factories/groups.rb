aeb3e0f285f413c9381456b763a93485690b8964
yudaionodera <yuudai.2451@gmail.com>
2017-11-27 19:48:39 +0900

    Letを使って、変数定義

---
+++ spec/factories/groups.rb
@@ -0,0 +1,9 @@
+require "Faker"
+
+FactoryGirl.define do
+  factory :group do
+    name         Faker::Name.name
+    created_at   "2016-01-01T00:00:00Z"
+    updated_at   "2016-01-01T00:00:00Z"
+  end
+end
