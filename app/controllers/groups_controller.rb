eb0303d82a455d0dd6d18830f626f7ea49b68f1a
yudaionodera <yuudai.2451@gmail.com>
2017-12-20 18:23:15 +0900

    フォームを整えた

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -33,6 +33,14 @@ class GroupsController < ApplicationController
     @group.update(update_params)
   end
 
+  def search
+    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name,user_ids: [])
---
+++ app/views/groups/search.json.jbuilder
@@ -0,0 +1,5 @@
+json.array! @users do |product|
+  json.id
+  json.name
+  json.email
+end
