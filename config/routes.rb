fec4d6cc5eb9d9f865777bc98c60e74265616837
yudaionodera <yuudai.2451@gmail.com>
2017-12-31 18:54:08 +0900

    間違ったルーティングのHTTPメソッド指定を修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   # ログアウト
 devise_scope :social_account do
-  get 'sign_out', to: "sessions#destroy"
+  delete 'sign_out', to: "sessions#destroy"
 end
   # resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
